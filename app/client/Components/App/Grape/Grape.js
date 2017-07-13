c65ab9659b5dbc1554206d297ed035f721cc7e2a
scjang <wkdtjdcjf294@gmail.com>
2017-07-13 15:35:18 +0900

    add Footer

--- app/client/Components/App/Grape/Grape.js
+++ app/client/Components/App/Grape/Grape.js
@@ -8,7 +8,9 @@
 		hasTemplate: true,
 		createChildComponents: function () {
 			S.Components.App.Grape.callSuper(this, 'createChildComponents');
-			this.nav = new S.Components.App.Nav();
+			// this.nav = new S.Components.App.Nav();
+			this.nav = this.addChild(new S.Components.App.Nav());
+      // console.log(this.children[0].parent());
 		}
 	});
 })(S.$);
